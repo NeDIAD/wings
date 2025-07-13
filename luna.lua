33a96361763ee4610170efd3f36f35211ef935dc
NeDIAD <148752789+NeDIAD@users.noreply.github.com>
2025-07-13 19:31:54 +0300

    asdasd

--- luna.lua
+++ luna.lua
@@ -260,6 +260,7 @@ math.pulse = function(min, max, speed, time)
     return (max + min) / 2 + math.sin(time * speed) * (max - min) / 2
 end
 --#endregion
+
 luna.mods = { }
 
 luna.register = function(name, module)
